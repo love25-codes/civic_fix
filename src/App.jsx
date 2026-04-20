3dd6cf62d3d98fb8ae2a3f508721866b814b3a2e
Lovepreet Kaur <kaurg1294@gmail.com>
2026-04-20 22:31:33 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { AuthProvider } from "./context/AuthContext";
 import { Navbar } from "./components/Navbar";
 import Dashboard from "./pages/Dashboard";
 import Learn from "./pages/Learn";
-import Map from "./pages/Map";
 import Report from "./pages/Report";
 import Analytics from "./pages/Analytics";
 import { ReportProvider } from "./context/ReportContext";
@@ -19,7 +18,6 @@ export default function App() {
           <Routes>
             <Route path="/" element={<Learn />} />
             <Route path="/dashboard" element={<Dashboard />} />
-            {/* <Route path="/map" element={<Map />} /> */}
             <Route path="/report" element={<Report />} />
             <Route path="/analytics" element={<Analytics />} />
           </Routes>
